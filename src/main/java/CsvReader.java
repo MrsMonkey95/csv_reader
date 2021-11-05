bacf7d3ff0a4c3e50bc30aead1ad96b0ab58b821
MrsMonkey95 <r.engelmann@gmx.ch>
2021-11-05 12:18:48 +0100

    updated reader

--- src/main/java/CsvReader.java
+++ src/main/java/CsvReader.java
@@ -1,9 +1,42 @@
+import javax.net.ssl.HostnameVerifier;
+import java.io.*;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class CsvReader {
 
-
+/** Created by Rahel Engelmann on 05.11.2021 */
     public static void main(String[] args) {
+        String line = "";
+        String splitBy = ",";
+        try
+        {
+        /** parsing a CSV file into BufferedReader class constructor */
+            BufferedReader br = new BufferedReader(new FileReader("sfm_holder_report.csv"));
+            while ((line = br.readLine()) != null)   //returns a Boolean value
+            {
+                String[] index = line.split(splitBy);    // use comma as separator
 
-        System.out.println("Hello World!");
+                String address = index[0];
+                String holderAddress = address.substring(1, address.length() - 1);
+                String balance = index[1];
+                String holderBalance = balance.substring(1, balance.length() - 1);
+                String pendingUpdate = index[2];
+                String holderPendingUpdate = pendingUpdate.substring(1, pendingUpdate.length() - 1);
+
+
+
+                System.out.println("Address: " + holderAddress);
+                System.out.println("Balance: " + holderBalance);
+                System.out.println("Pending: " + holderPendingUpdate);
+                System.out.println("________________________________________");
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
